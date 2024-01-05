b7374f3f89564b538767677c679be6853ae5b76b
Shashank Bisht <105352161+Shashank-bisht@users.noreply.github.com>
2024-01-05 23:58:16 +0530

    done with insert, delete

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 const Books = () => {
   const [books, setBooks] = useState([]);
@@ -8,14 +9,39 @@ const Books = () => {
       try {
         const res = await axios.get("http://localhost:8080/books");
         // const data = await res.json()
-        console.log(res.data);
+        setBooks(res.data);
       } catch (err) {
         console.log(err);
       }
     };
     fetchAllBooks();
   }, []);
-  return <div>Books</div>;
+ 
+  const handleDelete = async (id)=>{
+      try {
+        await axios.delete(`http://localhost:8080/books/${id}`);
+        window.location.reload();
+      } catch (error) {
+        console.log(error)
+      }
+  }
+
+  return <div>
+    <h1>Shanky</h1>
+    <div className="books">
+        {books.map((book) => (
+            <div className="book" key={book.id}>
+            {book.cover &&<img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+            <span>{book.price}</span>
+            <button className="delete" onClick={()=>handleDelete(book.id)}>Delete</button>
+            <button className="update">Update</button>
+            </div>  
+        ))}
+    </div>
+    <button className="btn"><Link className="link" to="/add">Add new book</Link></button>
+  </div>;
 };
 
 export default Books;
